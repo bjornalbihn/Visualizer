a26eb36b39505ca746e5a00d4dc95c6d987a2235
Jonatan Crafoord <Jonatan Crafoord>
2016-11-11 11:30:03 +0100

    Added script that feeds audio source with mic input.

---
+++ Vizualizer/Assets/4_Scripts/MicrophoneInput.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneInput : MonoBehaviour {
+
+    private float m_timeOut = 3;
+
+    void Start()
+    {
+        InitiateMicInput();
+    }
+
+    void InitiateMicInput()
+    {
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+
+        if (audioSource.clip == null)
+        {
+            audioSource.playOnAwake = false;
+            audioSource.Stop();
+            audioSource.loop = true;
+            audioSource.spatialBlend = 0.0f;
+            audioSource.clip = Microphone.Start("", true, 1, 44100);
+            if (audioSource.clip == null)
+            {
+                Debug.LogWarning("Failed to initiate microphone.");
+                return;
+            }
+
+            float timeOutTimer = Time.realtimeSinceStartup;
+            while (Microphone.GetPosition("") <= 0)
+            {
+                if (Time.realtimeSinceStartup > timeOutTimer + m_timeOut)
+                {
+                    Debug.LogWarning("Initiating microphone timed out.");
+                    return;
+                }
+            }
+
+            audioSource.Play();
+        }
+    }
+}
